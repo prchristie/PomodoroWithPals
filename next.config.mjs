b6c20772ed30ff4b26f62d2d849a4220a51167de
Patrick Christie <patrickc@canva.com>
2024-04-27 20:56:37 +1000

    Added type checking for environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+
 const nextConfig = {};
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+jiti("./src/app/utils/env.ts");
 
 export default nextConfig;
